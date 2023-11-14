9f8a8bce6581fbcdf8b4d94a377046d2953279be
lepenino1995 <lepenino@gmail.com>
2023-11-14 17:03:26 -0300

    Updates

---
+++ individual/individual/individual.js
@@ -0,0 +1,112 @@
+import OpenAI from "openai";
+import { Parser } from "@json2csv/plainjs";
+
+const keywordsInput = document.querySelector("#keywords");
+const idioma = document.querySelector('[name="idioma"]');
+const generateButton = document.querySelector("#generate");
+const model = document.querySelector("#model");
+
+const apiKeyValue = localStorage.getItem("openaiApiKey");
+
+const openai = new OpenAI({
+  apiKey: apiKeyValue,
+  dangerouslyAllowBrowser: true,
+});
+
+generateButton.addEventListener("click", async () => {
+  const keywordsList = keywordsInput.value.split("\n");
+  // const keywordsText = keywordsList.join(", ");
+  const idiomaValue = idioma.value;
+
+  let data = [];
+
+  for (let i = 0; i < keywordsList.length; i++) {
+    const result = await textGenerator(keywordsList[i], idiomaValue);
+    data.push(result);
+  }
+
+  downloadCSV(data);
+});
+
+async function textGenerator(keywordText, idiomaValue) {
+  const text = [];
+  let cleanedArticle = "";
+
+  const stream = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `Genera un articulo basado en estas palabras claves: ${keywordText} y en este lenguaje ${idiomaValue} en formato HTML. No incluyas etiquetas img o video.`,
+      },
+    ],
+    model: model.value,
+    temperature: 0.7,
+    stream: true,
+    // max_tokens: 100,
+  });
+
+  for await (const part of stream) {
+    // console.log(part.choices[0]?.delta?.content);
+    text.push(part.choices[0]?.delta?.content);
+
+    const article = document.querySelector("#article");
+    const completedText = text.join("");
+    cleanedArticle = completedText.replace(/\n/g, " ");
+    // cleanedArticle = completedText.replace(/[^<>]+/g, "");
+    article.innerHTML = cleanedArticle;
+  }
+
+  let descriptionText = [];
+  let cleanedDescription = "";
+  const descriptionStream = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `Genera una meta descripcion para un articulo basado en estas palabras claves: ${keywordText} y en este lenguaje ${idiomaValue} en formato de texto`,
+      },
+    ],
+    model: model.value,
+    temperature: 0.7,
+    stream: true,
+  });
+
+  // console.log(descriptionStream);
+
+  for await (const part of descriptionStream) {
+    descriptionText.push(part.choices[0]?.delta?.content);
+
+    const description = document.querySelector("#description");
+    const completedText = descriptionText.join("");
+    cleanedDescription = completedText.replace(/\n/g, " ");
+    description.innerHTML = cleanedDescription;
+  }
+
+  return {
+    palabra: keywordText,
+    articulo: cleanedArticle,
+    description: cleanedDescription,
+  };
+}
+
+function downloadCSV(rows) {
+  try {
+    const parser = new Parser();
+    const csv = parser.parse(rows);
+
+    console.log(csv);
+
+    // download csv file
+    const downloadLink = document.createElement("a");
+    const blob = new Blob(["\ufeff", csv]);
+    const url = URL.createObjectURL(blob);
+    downloadLink.href = url;
+    downloadLink.download = "data.csv";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+    // document.body.removeChild(downloadLink);
+  } catch (err) {
+    console.error(err);
+  }
+}
---
+++ politicas/politicas/politicas.js
@@ -0,0 +1,60 @@
+import { downloadCSV } from "../libs/downloadCSV.js";
+import { generatePolicy } from "../libs/generatePolicy.js";
+
+const btnGenerate = document.querySelector("#btnGenerate");
+
+window.addEventListener("DOMContentLoaded", async (event) => {
+  const res = await fetch("/data/countries.json");
+  const data = await res.json();
+  console.log(data);
+
+  const countries = data.map((country) => {
+    return `<option value="${country.name}">${country.name}</option>`;
+  });
+
+  country.innerHTML = countries.join("");
+});
+
+btnGenerate.addEventListener("click", async () => {
+  const lang = document.querySelector('[name="idioma"]:checked').value;
+
+  const companyName = document.querySelector("#companyName").value;
+  const websiteURL = document.querySelector("#websiteURL").value;
+  const websiteName = document.querySelector("#websiteName").value;
+  const companyEmail = document.querySelector("#companyEmail").value;
+
+  console.log(companyName, websiteURL, websiteName, companyEmail);
+
+  // Generate Policy
+  const conditions = await generatePolicy("conditions", {
+    companyName,
+    webName: websiteName,
+    webURL: websiteURL,
+    companyEmail,
+  });
+  const cookies = await generatePolicy("cookies", {
+    companyName,
+    webName: websiteName,
+    webURL: websiteURL,
+    companyEmail,
+  });
+  const privacy = await generatePolicy("privacy", {
+    companyName,
+    webName: websiteName,
+    webURL: websiteURL,
+    companyEmail,
+  });
+
+  const policyTitle = lang === "es" ? "Politicas" : "Policies";
+  const cookiesTitle = lang === "es" ? "Cookies" : "Cookies";
+  const privacyTitle = lang === "es" ? "Privacidad" : "Privacy";
+
+  downloadCSV(
+    {
+      [policyTitle]: conditions,
+      [cookiesTitle]: cookies,
+      [privacyTitle]: privacy,
+    },
+    "politicas"
+  );
+});
