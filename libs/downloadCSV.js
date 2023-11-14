77185fd66d2da32415252c276496c7e17d6d5894
lepenino1995 <lepenino@gmail.com>
2023-11-14 17:33:46 -0300

    Updates

---
+++ libs/downloadCSV.js
@@ -0,0 +1,22 @@
+// import { Parser } from "@json2csv/plainjs";
+
+import {
+  Parser
+} from 'https://cdn.jsdelivr.net/npm/@json2csv/plainjs@7.0.3/+esm'
+
+
+export function downloadCSV(rows, name = "data") {
+  try {
+    const parser = new Parser();
+    const csv = parser.parse(rows);
+    const downloadLink = document.createElement("a");
+    const blob = new Blob(["\ufeff", csv]);
+    const url = URL.createObjectURL(blob);
+    downloadLink.href = url;
+    downloadLink.download = `${name}.csv`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+  } catch (err) {
+    console.error(err);
+  }
+}
