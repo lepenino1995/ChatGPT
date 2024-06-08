49d67cd1d1a00f164a253087c6cf836ccb5e657f
Leandro <leandro@Ordenador-portatil-de-Leandro.local>
2024-06-07 22:32:02 -0400

    newall-1

---
+++ newall/newall.js
@@ -0,0 +1,36 @@
+import OpenAI from "https://cdn.jsdelivr.net/npm/openai@4.17.5/+esm";
+import { downloadCSV } from "../libs/downloadCSV.js";
+import { textGenerator } from "../libs/textGenerator.js";
+
+const keywordsInput = document.querySelector("#keywords");
+const generateButton = document.querySelector("#generate");
+const model = document.querySelector("#model");
+
+const apiKeyValue = localStorage.getItem("openaiApiKey");
+
+const openai = new OpenAI({
+  apiKey: apiKeyValue,
+  dangerouslyAllowBrowser: true,
+});
+
+generateButton.addEventListener("click", async () => {
+  const keywordsList = keywordsInput.value.split("\n");
+  // const keywordsText = keywordsList.join(", ");
+  // get of checkbox
+  const idioma = document.querySelector('input[name="idioma"]:checked');
+
+  const idiomaValue = idioma.value;
+  console.log(idiomaValue);
+
+  const language = idiomaValue === "es" ? "español" : "ingles";
+  console.log(language);
+
+  let data = [];
+
+  for (let i = 0; i < keywordsList.length; i++) {
+    const result = await textGenerator(keywordsList[i], language);
+    data.push(result);
+  }
+
+  downloadCSV({}, data, "articulo");
+});
