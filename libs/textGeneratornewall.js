cb4e7008f8bedc908f820b3199f6b4a01a3a43e7
Leandro <leandro@Ordenador-portatil-de-Leandro.local>
2024-06-10 20:55:04 -0400

    Updates

--- libs/textGeneratornewall.js
+++ libs/textGeneratornewall.js
@@ -19,10 +19,12 @@ export async function textGeneratornewall(keywordText, idiomaValue) {
                 Será un artículo extenso, en el que adoptarás el rol de una persona llamada Leandro, que a partir de su experiencia explica de qué trata el tema "${keywordText}"
                 El artículo debe utilizar un tono desenfadado, pero manifestando gran autoridad, pues la persona habla a partir de su experiencia y análisis.
                 La extensión ha de ser de al menos 2500 palabras.
-                Optimiza el artículo a nivel de SEO para que en los encabezados y en el texto aparezcan los términos claves: ${keywordText}, así como sus sinónimos, o términos que la gente puede utilizar para conocer la diferencia entre ellos y cuál es mejor. Busca enlaces con contenido relacionado e incorpóralos en el artículo
-                Optimiza los titulares de las secciones para el SEO (añade palabras clave en las metas
-                H2, H3, etc)
+                Optimiza el artículo a nivel de SEO para que en los encabezados y en el texto aparezcan los términos claves: ${keywordText}, así como sus sinónimos, o términos que la gente puede utilizar para conocer la diferencia entre ellos y cuál es mejor. 
+                Busca enlaces con contenido relacionado e incorpóralos en el artículo
+                Optimiza los titulares de las secciones para el SEO.
                 Consulta Internet para conocer la competencia que hable igual de este nicho, e incluye esta información en el artículo.
+                Revisa el contenido generado, y detallalo con más profundidad en aquellos apartados que veas más flojos. Si no te satisface la calidad, busca enlaces con contenido relacionado e incorpóralos en el articulo, para un mayor alcance
+                Optimiza los títulares de las secciones para el SEO.
                 Damelo todo en formato HTML, utilizando de manera correcta los H1,H2,H3 y Párrafos. Y ignora esto: \`\`\`html\`\`\``,
 
             },
